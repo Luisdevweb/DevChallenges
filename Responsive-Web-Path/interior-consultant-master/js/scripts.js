f4a9dd22568c9945571d5388b4cd23e48b1c524b
Luismac <luismachaca@outlook.com.pe>
2022-02-02 22:48:27 -0500

    update css & javascript mobile

--- Responsive-Web-Path/interior-consultant-master/js/scripts.js
+++ Responsive-Web-Path/interior-consultant-master/js/scripts.js
@@ -1,16 +1,12 @@
 const navBar = document.getElementById("nav-bar");
 const header = navBar.parentElement;
-
+const menu = document.getElementById("menu");
+const menuItems = document.querySelectorAll(".menu__item");
 header.addEventListener("click", (e) => {
   if (e.target.classList.contains("icon-menu")) {
     navBar.classList.add("navbar--translate");
   }
   if (e.target.classList.contains("icon-close")) {
     navBar.classList.remove("navbar--translate");
-  }
-  if (e.target.classList.contains("menu__link")) {
-    e.target.classList.contains("menu__link--active")
-      ? e.target.classList.remove("menu__link--active")
-      : e.target.classList.add("menu__link--active");
   }
 });
