deb95dca823a2c32ccbd9c274c9de7be127dc6e8
Luismac <luismachaca@outlook.com.pe>
2022-02-26 20:12:53 -0500

    increase and decrease the number of items

--- Responsive-Web-Path/checkout-page-master/js/scripts.js
+++ Responsive-Web-Path/checkout-page-master/js/scripts.js
@@ -1,1 +1,24 @@
+const counter = document.querySelectorAll(".counter");
+let number = 1;
+counter.forEach((item) =>
+  item.addEventListener("click", (e) => {
+    if (e.target.classList.contains("button--decrease")) {
+      decreaseNumber(e);
+    }
+    if (e.target.classList.contains("button--increase")) {
+      increaseNumber(e);
+    }
+  })
+);
 
+const decreaseNumber = (e) => {
+  const counterNumber = e.target.parentElement.nextElementSibling;
+  number > 1 ? number-- : (number = 1);
+  counterNumber.textContent = number;
+};
+
+const increaseNumber = (e) => {
+  const counterNumber = e.target.parentElement.previousElementSibling;
+  number >= 1 ? number++ : (number = 1);
+  counterNumber.textContent = number;
+};
